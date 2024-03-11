caa6edab20aad4b1995ffd9074f066e233a607a7
Emma Secrest <esecrest908@gmail.com>
2024-03-11 16:21:58 -0400

    all test finished

--- tree-node/test/unit/model/tree.test.ts
+++ tree-node/test/unit/model/tree.test.ts
@@ -41,9 +41,54 @@ test('find longest tree paths for tree1', async () => {
 });
 
 test('find longest tree paths for tree2', async () => {
-  // todo: implement this
+  const response =  traverseTree(tree2);
+  expect(response[0]).toEqual(expect.arrayContaining([
+    "tree2", 
+    "tree2.b", 
+    "tree2.b.c", 
+    "tree2.b.c.b", 
+    "tree2.b.c.b.a", 
+    "tree2.b.c.b.a.b", 
+    "tree2.b.c.b.a.b.a", 
+    "tree2.b.c.b.a.b.a.b", 
+    "tree2.b.c.b.a.b.a.b.a", 
+    "tree2.b.c.b.a.b.a.b.a.a"]  ))
+    expect(response.length).toBe(1);
+
 });
 
 test('find longest tree paths for tree3', async () => {
-  // todo: implement this
+  const response =  traverseTree(tree3);
+  expect(response[0]).toEqual(expect.arrayContaining([
+    "tree3", 
+    "tree3.b", 
+    "tree3.b.c", 
+    "tree3.b.c.b", 
+    "tree3.b.c.b.a", 
+    "tree3.b.c.b.a.a"]))
+
+    expect(response[1]).toEqual(expect.arrayContaining([
+      "tree3", 
+      "tree3.b", 
+      "tree3.b.c", 
+      "tree3.b.c.b", 
+      "tree3.b.c.b.a", 
+      "tree3.b.c.b.a.b"]))
+
+    expect(response[2]).toEqual(expect.arrayContaining([
+      "tree3", 
+      "tree3.c", 
+      "tree3.c.c", 
+      "tree3.c.c.b", 
+      "tree3.c.c.b.a", 
+      "tree3.c.c.b.a.a"]))
+      expect(response[3]).toEqual(expect.arrayContaining( [
+        "tree3", 
+        "tree3.c", 
+        "tree3.c.c", 
+        "tree3.c.c.b", 
+        "tree3.c.c.b.a", 
+        "tree3.c.c.b.a.b"]))
+      expect(response.length).toBe(4);
+
 });
